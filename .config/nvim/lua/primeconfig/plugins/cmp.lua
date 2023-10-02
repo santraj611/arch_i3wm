431ddac8fd2ed085b209b1f881ac4b5877391818
santraj611 <santraj611@gmail.com>
2023-10-02 14:05:54 +0530

    lsp and cmp has fixed

--- .config/nvim/lua/primeconfig/plugins/cmp.lua
+++ .config/nvim/lua/primeconfig/plugins/cmp.lua
@@ -4,9 +4,11 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
+      ["<C-k>"] = cmp.mapping.select_prev_item(),
+	  ["<C-j>"] = cmp.mapping.select_next_item(),
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-o>'] = cmp.mapping.complete(),
+      ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
@@ -15,48 +17,15 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body)
     end,
   },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' },  -- For vsnip users.
+    -- { name = 'vsnip' },  -- For vsnip users.
     { name = 'luasnip' },
-  }, {
     { name = 'buffer' },
-  }),
+    { name = 'path' },
+    })
 })
-
-
-vim.o.completeopt = "menuone,noselect"
---
--- require('compe').setup {
---   enabled = true;
---   autocomplete = true;
---   debug = false;
---   min_length = 1;
---   preselect = 'enable';
---   throttle_time = 80;
---   source_timeout = 200;
---   resolve_timeout = 800;
---   incomplete_delay = 400;
---   max_abbr_width = 100;
---   max_kind_width = 100;
---   max_menu_width = 100;
---   documentation = {
---     border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
---     winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
---     max_width = 120,
---     min_width = 60,
---     max_height = math.floor(vim.o.lines * 0.3),
---     min_height = 1,
---   };
---
---   source = {
---     path = true;
---     buffer = true;
---     calc = true;
---     nvim_lsp = true;
---     nvim_lua = true;
---     vsnip = true;
---     ultisnips = true;
---     luasnip = true;
---   };
--- }
--- .config/nvim/lua/primeconfig/plugins/lsp.lua
+++ .config/nvim/lua/primeconfig/plugins/lsp.lua
@@ -6,32 +6,38 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
+    ensure_installed = {'lua_ls', 'tsserver', 'rust_analyzer', 'pyright', 'clangd'},
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
-require("lspconfig").lua_ls.setup {
-    capabilities = capabilities
-}
-require("lspconfig").rust_analyzer.setup {
-    capabilities = capabilities
-}
-require("lspconfig").pyright.setup {
-    capabilities = capabilities,
-    cmd = { "pyright-langserver", "--stdio" },
-    filetypes = { "python" },
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "openFilesOnly",
-                useLibraryCodeForTypes = true
-            }
-        }
-    },
-    single_file_support = true,
+lsp_servers = {'lua_ls', 'rust_analyzer', 'pyright', 'tsserver', 'clangd'}
+lsp_zero.setup_servers(lsp_servers)
 
-}
+-- require("lspconfig").lua_ls.setup {
+--     capabilities = capabilities
+-- }
+-- require("lspconfig").rust_analyzer.setup {
+--     capabilities = capabilities
+-- }
+-- require("lspconfig").pyright.setup {
+--     capabilities = capabilities,
+--     cmd = { "pyright-langserver", "--stdio" },
+--     filetypes = { "python" },
+--     settings = {
+--         python = {
+--             analysis = {
+--                 autoSearchPaths = true,
+--                 diagnosticMode = "openFilesOnly",
+--                 useLibraryCodeForTypes = true
+--             }
+--         }
+--     },
+--     single_file_support = true,
+--
+-- }
